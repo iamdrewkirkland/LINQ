da02ef1e9be0b245b25349f665341bb79b739a87
Drew Kirkland <drew.kirkland@gmail.com>
2020-07-29 12:21:43 -0400

    Setup LinkController method to get links by current user id.

--- Linq/Linq/Controllers/LinkController.cs
+++ Linq/Linq/Controllers/LinkController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Linq.Data;
+using Linq.Models;
+using Linq.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +15,28 @@ namespace Linq.Controllers
     [ApiController]
     public class LinkController : ControllerBase
     {
+
+        private readonly LinkRepository _linkRepository;
+        private readonly UserProfileRepository _userProfileRepository;
+        
+        public LinkController(ApplicationDbContext context)
+        {
+            _linkRepository = new LinkRepository(context);
+            _userProfileRepository = new UserProfileRepository(context);
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var currentUser = GetCurrentUserProfile();
+            return Ok(_linkRepository.GetByUserId(currentUser.Id));
+        }
+
+        private UserProfile GetCurrentUserProfile()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+        }
     }
 }
--- Linq/Linq/Repositories/LinkRepository.cs
+++ Linq/Linq/Repositories/LinkRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Linq.Data;
+using Linq.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,34 @@ namespace Linq.Repositories
 {
     public class LinkRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public LinkRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //public List<Link> GetAll() 
+        //{
+        //    return _context.Links
+        //                .Include(l => l.Category)
+        //                .Include(l => l.UserProfile)
+        //                .ToList();
+        //}
+
+        public List<Link> GetByUserId(int id)
+        { 
+            return _context.Links
+                            .Include(l => l.Category)
+                            .Include(l => l.UserProfile)
+                            .Where(l => l.UserProfileId == id)
+                            .OrderByDescending(l => l.CreateDate)
+                            .ToList();
+        }
+            
+
+
+
     }
 }
