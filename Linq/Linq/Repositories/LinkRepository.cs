77b7c4bae2dfae92c745fa78a7d81a63792e7ba7
Drew Kirkland <drew.kirkland@gmail.com>
2020-07-29 12:11:37 -0400

    Added repo method for getting link by current user.

---
+++ Linq/Linq/Repositories/LinkRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linq.Repositories
+{
+    public class LinkRepository
+    {
+    }
+}
